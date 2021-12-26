794c97d8a290b6e8132ba1a3ddb8cd52c7d26ec3
nikhil <knikhil.2514@gmail.com>
2021-12-26 14:13:30 +0530

    updated the program to get user input

--- tables.go
+++ tables.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main(){
+	fmt.Print("Enter table number: ")
+	var tableNo int
+	fmt.Scanln(&tableNo)
+	fmt.Println(" ")
 	for i:=0;i<=10;i++{
-		fmt.Println("2*i=",%i,2*i)
-	}
+		fmt.Printf("%d * %d = %d \n",tableNo,i,tableNo*i)
+	}    
+	
 }
+
+
+
 
